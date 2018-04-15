0ac88d57b7e679b9abaf42e760625a22ac179d88
Cyril Do <cyrildo88@gmail.com>
2018-04-15 03:26:21 +0200

    first commit

---
+++ cart.php
@@ -0,0 +1,84 @@
+<?php
+
+
+
+require 'inc/head.php'; ?>
+<section class="cookies container-fluid">
+    <div class="row">
+      <!-- TODO : Display shopping cart items from $_COOKIES here. -->
+        <?php
+        if (isset($_SESSION['username'])) {
+
+            $products = array("0", "Pecan nuts", "Chocolate chips", "Chocolate cookies", "M&M's cookies");
+
+            if (!isset($_SESSION["total"])) {
+                $_SESSION["total"] = 0;
+                for ($i = 0; $i < count($products); $i++) {
+                    $_SESSION["qty"][$i] = 0;
+                }
+            }
+
+            //Add
+            if (isset($_GET["add"])) {
+                $i = $_GET["add"];
+                $qty = $_SESSION["qty"][$i] + 1;
+                $_SESSION["cart"][$i] = $i;
+                $_SESSION["qty"][$i] = $qty;
+            }
+            //---------------------------
+            //Delete
+            if (isset($_GET["delete"])) {
+                $i = $_GET["delete"];
+                $qty = $_SESSION["qty"][$i];
+                $qty--;
+                $_SESSION["qty"][$i] = $qty;
+                //remove item if quantity is zero
+                if ($qty == 0) {
+                    unset($_SESSION["cart"][$i]);
+                }
+            }
+
+            if (isset($_SESSION["cart"])) {
+                ?>
+                <div class="container">
+                <h2>Cart</h2>
+                <table>
+                    <tr>
+                        <th>Product</th>
+                        <th width="10px">&nbsp;</th>
+                        <th>Qty</th>
+                        <th width="10px">&nbsp;</th>
+                        <th>Action</th>
+                    </tr>
+                    <?php
+                    $total = 0;
+                    foreach ($_SESSION["cart"] as $i) {
+                        ?>
+                        <tr>
+                            <td><?php echo($products[$_SESSION["cart"][$i]]); ?></td>
+                            <td width="10px">&nbsp;</td>
+                            <td><?php echo($_SESSION["qty"][$i]); ?></td>
+                            <td width="10px">&nbsp;</td>
+                            <td><a href="?delete=<?php echo($i); ?>">Delete from cart</a></td>
+                        </tr>
+                        <?php
+                        $total = $total + $_SESSION["qty"][$i];
+                    }
+                    $_SESSION["total"] = $total;
+
+
+                    ?>
+
+                    <tr>
+                        <td colspan="7">Total : <?php echo($total); ?></td>
+                    </tr>
+                </table>
+                </div>
+                <?php
+            }
+        } else { echo "<h1>You must log in to order our cookies, please connect !!</h1>";
+        }
+        ?>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+
+require 'inc/head.php'; ?>
+<section class="cookies container-fluid">
+  <div class="row">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <figure class="thumbnail text-center">
+        <img src="assets/img/product-46.jpg" alt="cookies choclate chips" class="img-responsive">
+        <figcaption class="caption">
+          <h3>Pecan nuts</h3>
+          <p>Cooked by Penny !</p>
+          <a name="pecan" href="cart.php?add=1" class="btn btn-primary" type="submit">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+          </a>
+        </figcaption>
+      </figure>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <figure class="thumbnail text-center">
+        <img src="assets/img/product-36.jpg" alt="cookies choclate chips" class="img-responsive">
+        <figcaption class="caption">
+          <h3>Chocolate chips</h3>
+          <p>Cooked by Bernadette !</p>
+          <a name="chocochips" href="cart.php?add=2" class="btn btn-primary" type="submit">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+          </a>
+        </figcaption>
+      </figure>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <figure class="thumbnail text-center">
+        <img src="assets/img/product-58.jpg" alt="cookies choclate chips" class="img-responsive">
+        <figcaption class="caption">
+          <h3>Chocolate cookie</h3>
+          <p>Cooked by Bernadette !</p>
+          <a name="chococook" href="cart.php?add=3" class="btn btn-primary" type="submit">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+          </a>
+        </figcaption>
+      </figure>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <figure class="thumbnail text-center">
+        <img src="assets/img/product-32.jpg" alt="cookies choclate chips" class="img-responsive">
+        <figcaption class="caption">
+          <h3>M&M's&copy; cookies</h3>
+          <p>Cooked by Penny !</p>
+          <a name="mms" href="cart.php?add=4" class="btn btn-primary" type="submit">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+          </a>
+        </figcaption>
+      </figure>
+    </div>
+  </div>
+</section>
+<?php require 'inc/foot.php'; ?>
